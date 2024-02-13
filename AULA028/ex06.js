0aeda93dc630d4f2da7374f3a3f62c3596922f8e
dionatamartini <dionatamartinii@hotmail.com>
2024-02-13 14:02:17 -0300

    ex06

--- AULA028/ex06.js
+++ AULA028/ex06.js
@@ -1,1 +1,22 @@
-let dinheiro = prompt("Informe a quantidade dinheiro disponível:")
+let dinheiro = prompt("Informe o dinheiro disponível")
+dinheiro = parseFloat(dinheiro)
+opcao = ""
+
+do {
+    opcao = prompt(`O saldo disponível no momento é de R$ ${dinheiro}\n\nEscolha uma das opções abaixo:\n\n1.Adicionar\n2.Remover\n3.Sair`)
+    
+
+    switch(opcao) {
+        case "1":
+            dinheiro += parseFloat(prompt("Informe o valor que deseja adicionar:"))
+            break
+        case "2":
+            dinheiro -= prompt("Informe o calor que deseja subtrair")
+            break
+        case "3":
+            alert("Saindo...")
+            break
+        default:
+        alert("[ERRO] Opção inválida")
+    }
+} while (opcao !== "3")
