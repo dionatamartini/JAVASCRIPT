c7caa32bd3a65373e265e81492ceb56d9592509d
dionatamartini <dionatamartinii@hotmail.com>
2024-03-02 14:27:07 -0300

    hofs

--- AULAhigh-order/script.js
+++ AULAhigh-order/script.js
@@ -1,5 +1,5 @@
 //SORT
-
+/*
 const personagens = [
     { nivel: 42, nome: "Thrall", raca: "Orc", classe: "Xamã" },
     { nivel: 28, nome: "Garrosh", raca: "Orc", classe: "Guerreiro" },
@@ -15,3 +15,38 @@ personagens.sort(function (a, b) {
 })
 
 console.log(personagens)
+
+function exibirElemento(elemento, indice, array){
+  console.log({
+    array,
+    elemento,
+    indice
+  })
+}
+
+const personagens = ["maça" , "laranja" , "melancia" , "limão"]
+
+
+personagens.forEach(function (elemento, indice, array){
+  console.log({
+    elemento,
+    indice,
+    array
+  })
+})*/
+
+const personagens = [
+  { nivel: 42, nome: "Thrall", raca: "Orc", classe: "Xamã" },
+  { nivel: 28, nome: "Garrosh", raca: "Orc", classe: "Guerreiro" },
+  { nivel: 35, nome: "Varok", raca: "Orc", classe: "Guerreiro" },
+  { nivel: 35, nome: "Uther", raca: "Humano", classe: "Paladino" },
+  { nivel: 26, nome: "Jaina", raca: "Humano", classe: "Maga" },
+  { nivel: 39, nome: "Tyrande", raca: "Elfo Noturno", classe: "Sacerdotisa" },
+  { nivel: 29, nome: "Muradin", raca: "Anão", classe: "Guerreiro" },
+]
+
+const nomes = personagens.map(function (personagem) {
+  return personagem.nome
+})
+
+console.log(nomes)
