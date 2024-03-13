74b39896e406907c5ec14fde34b2614c7431ecb3
dionatamartini <dionatamartinii@hotmail.com>
2024-03-13 10:58:08 -0300

    conclusão-ex-escalação

--- EX-ESCALACAO-TIME/script.js
+++ EX-ESCALACAO-TIME/script.js
@@ -1,65 +1,31 @@
-let listaVisivel = false;  
-const player = [] 
+function addPlayer() {
+    const position = document.getElementById('position').value
+    const name = document.getElementById('name').value
+    const number = document.getElementById('number').value
 
+    const confirmation = confirm(`Escalar ${name} com ${position}?`)
 
-function escalarJogador() {
-    const escalar = document.getElementById('escalar');
+    if (confirmation) {
+        const teamList = document.getElementById('teamList')
+        const playerItem = document.createElement('li')
+        playerItem.id = `player-${number}`
+        playerItem.innerText = `${position}: ${name} (${number})`
+        teamList.appendChild(playerItem)
+
+        document.getElementById('position').value = ''
+        document.getElementById('name').value = ''
+        document.getElementById('number').value = ''
+    }
+}
+
+function removePlayer() {
+    const number = document.getElementById('numberToRemove').value
+    const playerToRemove = document.getElementById(`player-${number}`)
     
+    const confirmation = confirm(`Remover o jogador? ${playerToRemove.innerText}?`)
 
-    if (!listaVisivel) {
-        const li = ['Informe a posição: ', 'Informe o nome: ', 'Informe o número da camisa: '];
-        const ul = document.createElement('ul');
-        ul.className = 'ul-list';
-
-        const buttonYes = document.createElement('button')
-        buttonYes.innerText = 'Sim'
-        buttonYes.className = 'button-yes'
-
-        li.forEach(liText => {
-            const newLi = document.createElement('li');
-            const label = document.createElement('label');
-            
-            label.textContent = liText;
-
-            const input = document.createElement('input');
-            input.type = 'text';
-
-        
-            // Adiciona um quebra de linha para separar os elementos
-            newLi.appendChild(document.createElement('br'));
-
-            newLi.appendChild(label);
-            newLi.appendChild(input);
-
-            ul.appendChild(newLi);
-
-            label.addEventListener('click', function() {
-                input.focus();
-            });
-
-});
-        escalar.appendChild(ul);
-        escalar.appendChild(buttonYes);
-
-        buttonYes.addEventListener('click', function() {
-            const inputs = escalar.querySelectorAll('.ul-list li input'); // Seleciona todos os inputs dentro da lista criada
-            inputs.forEach(input => {
-                const labelText = input.previousElementSibling.textContent; // Obtém o texto do label correspondente
-                const inputValue = input.value; // Obtém o valor do input
-                player[labelText] = inputValue; // Adiciona o valor do input ao objeto player, usando o texto do label como chave
-            });
-            console.log(player);
-        });
-
-
-
-        listaVisivel = true; // Atualiza o estado da lista para visível
-    } else {
-        const buttonYes = document.querySelector('.button-yes')
-        const ul = document.querySelector('.ul-list');
-        escalar.removeChild(buttonYes)
-        escalar.removeChild(ul); // Remove a lista
-        
-        listaVisivel = false; // Atualiza o estado da lista para invisível
+    if (confirmation) {
+        document.getElementById('teamList').removeChild(playerToRemove)
+        document.getElementById('numberToRemove').value = ''
     }
 }
