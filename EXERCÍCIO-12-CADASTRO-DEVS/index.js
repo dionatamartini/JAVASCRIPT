9bda075a759cd77ae6e757f5d6ad14656038baa2
dionatamartini <dionatamartinii@hotmail.com>
2024-03-15 11:16:07 -0300

    ex12

--- EXERCÍCIO-12-CADASTRO-DEVS/index.js
+++ EXERCÍCIO-12-CADASTRO-DEVS/index.js
@@ -1,71 +1,66 @@
-const inicio = document.getElementById('inicio')
-const button = document.getElementById('button')
+const inicio = document.getElementById('inicio');
+const button = document.getElementById('button');
+let contador = 1;
 
-button.addEventListener('click' , function (ev) {
-    const name = document.querySelector('label')
-    const input = document.querySelector('input')
+button.addEventListener('click', function(ev) {
+
+    for(i = 0; i < 2; i++) {
+        const lineBreak = document.createElement('br')
+        inicio.appendChild(lineBreak)
+    }
+    const newLabel = document.createElement('label');
+    newLabel.setAttribute('for', 'tecnologia' + contador);
+    newLabel.innerText = 'Informe a tecnologia: ';
+
+    const newInput = document.createElement('input');
+    newInput.type = 'text';
+    newInput.id = 'textTecnologia' + contador;
+    newInput.name = 'textTecnologia' + contador;
+
+    const inputRadio2anos = document.createElement('input');
+    inputRadio2anos.type = 'radio';
+    inputRadio2anos.id = '2anos' + contador;
+    inputRadio2anos.name = '2anos' + contador;
+    inputRadio2anos.value = '2anos' + contador;
+
+    const labelRadio2anos = document.createElement('label');
+    labelRadio2anos.setAttribute('for', 'radio2anos' + contador);
+    labelRadio2anos.innerText = '0 a 2 anos';
+
+    const inputRadio3a4anos = document.createElement('input');
+    inputRadio3a4anos.type = 'radio';
+    inputRadio3a4anos.id = '3a4anos' + contador;
+    inputRadio3a4anos.name = '3a4anos' + contador;
+    inputRadio3a4anos.value = '3a4anos' + contador;
+
+    const labelRadio3a4anos = document.createElement('label');
+    labelRadio3a4anos.setAttribute('for', 'radio3a4anos' + contador);
+    labelRadio3a4anos.innerText = '3 a 4 anos';
+
+    const inputRadio5anos = document.createElement('input');
+    inputRadio5anos.type = 'radio';
+    inputRadio5anos.id = '+5anos' + contador;
+    inputRadio5anos.name = '+5anos' + contador;
+    inputRadio5anos.value = '+5anos' + contador;
+
+    const labelRadio5anos = document.createElement('label');
+    labelRadio5anos.setAttribute('for', 'radio+5anos' + contador);
+    labelRadio5anos.innerText = '+5 de anos';
+
+    inicio.appendChild(newLabel);
+    inicio.appendChild(newInput);
 
     for (let i = 0; i < 2; i++) {
         const lineBreak = document.createElement('br');
         inicio.appendChild(lineBreak);
     }
 
-    newLabel = document.createElement('label')
-    newLabel.setAttribute('for' , 'tecnologia')
-    newLabel.id = 'tecnologia'
-    newLabel.innerText = 'Informe a tecnologia: '
+    inicio.appendChild(inputRadio2anos);
+    inicio.appendChild(labelRadio2anos);
+    inicio.appendChild(inputRadio3a4anos);
+    inicio.appendChild(labelRadio3a4anos);
+    inicio.appendChild(inputRadio5anos);
+    inicio.appendChild(labelRadio5anos);
 
-    newInput = document.createElement('input')
-    newInput.type = 'text'
-    newInput.id = 'textTecnologia'
-    newInput.name = 'textTecnologia'
-
-    inputRadio2anos = document.createElement('input')
-    inputRadio2anos.type = 'radio'
-    inputRadio2anos.id = '2anos'
-    inputRadio2anos.name = '2anos'
-    inputRadio2anos.value = '2anos'
-
-    labelRadio2anos = document.createElement('label')
-    labelRadio2anos.setAttribute('for' , 'radio2anos')
-    labelRadio2anos.id = 'radio2anos'
-    labelRadio2anos.innerText = '0 a 2 anos'
-
-    inputRadio3a4anos = document.createElement('input')
-    inputRadio3a4anos.type = 'radio'
-    inputRadio3a4anos.id = '3a4anos'
-    inputRadio3a4anos.name = '3a4anos'
-    inputRadio3a4anos.value = '3a4anos'
-
-    labelRadio3a4anos = document.createElement('label')
-    labelRadio3a4anos.setAttribute('for' , 'radio3a4anos')
-    labelRadio3a4anos.id = 'radio3a4anos'
-    labelRadio3a4anos.innerText = '3 a 4 anos'
-
-    inputRadio5anos = document.createElement('input')
-    inputRadio5anos.type = 'radio'
-    inputRadio5anos.id = '+5anos'
-    inputRadio5anos.name = '+5anos'
-    inputRadio5anos.value = '+5anos'
-
-    labelRadio5anos = document.createElement('label')
-    labelRadio5anos.setAttribute('for' , 'radio+5anos')
-    labelRadio5anos.id = 'radio+5anos'
-    labelRadio5anos.innerText = '+5 anos'
-
-    inicio.appendChild(newLabel)
-    inicio.appendChild(newInput)
-
-    for (let i = 0; i < 2; i++) {
-        const lineBreak02 = document.createElement('br');
-        inicio.appendChild(lineBreak02);
-    }
-
-    inicio.appendChild(inputRadio2anos)
-    inicio.appendChild(labelRadio2anos)
-    inicio.appendChild(inputRadio3a4anos)
-    inicio.appendChild(labelRadio3a4anos)
-    inicio.appendChild(inputRadio5anos)
-    inicio.appendChild(labelRadio5anos)
-    
-})
+    contador++;
+});
