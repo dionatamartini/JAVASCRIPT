934921d531ceeb5a3e9586e8f1e63948dd8e9c3a
dionatamartini <dionatamartinii@hotmail.com>
2024-03-23 10:40:36 -0300

    jogo da velha

--- EXERCÍCIOS/EX-JOGO-DA-VELHA/script.js
+++ EXERCÍCIOS/EX-JOGO-DA-VELHA/script.js
@@ -1,64 +1,93 @@
-const infoPlayers = document.getElementById('infoPlayers');
-const namePlayer1 = []
-const namePlayer2 = []
+// Variáveis globais úteis
+const boardRegions = document.querySelectorAll('#gameBoard span')
+let vBoard = []
+let turnPlayer = ''
 
-// Adiciona uma classe quando a página é carregada
-window.addEventListener('load', function() {
-    infoPlayers.classList.add('loginPlayers');
+function updateTitle() {
+  const playerInput = document.getElementById(turnPlayer)
+  document.getElementById('turnPlayer').innerText = playerInput.value
+}
 
-    const h1 = document.createElement('h1')
-    h1.classList = 'charKey'
-    h1.innerText = 'JOGO DA VELHA'
+function initializeGame() {
+  // Inicializa as variáveis globais 
+  vBoard = [['', '', ''], ['', '', ''], ['', '', '']]
+  turnPlayer = 'player1'
+  // Ajusta o título da página (caso seja necessário)
+  document.querySelector('h2').innerHTML = 'Vez de: <span id="turnPlayer"></span>'
+  updateTitle()
+  // Limpa o tabuleiro (caso seja necessário) e adiciona os eventos de clique
+  boardRegions.forEach(function (element) {
+    element.classList.remove('win')
+    element.innerText = ''
+    element.classList.add('cursor-pointer')
+    element.addEventListener('click', handleBoardClick)
+  })
+}
+// Verifica se existem três regiões iguais em sequência e devolve as regiões
+function getWinRegions() {
+  const winRegions = []
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2])
+    winRegions.push("0.0", "0.1", "0.2")
+  if (vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2])
+    winRegions.push("1.0", "1.1", "1.2")
+  if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2])
+    winRegions.push("2.0", "2.1", "2.2")
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0])
+    winRegions.push("0.0", "1.0", "2.0")
+  if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1])
+    winRegions.push("0.1", "1.1", "2.1")
+  if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2])
+    winRegions.push("0.2", "1.2", "2.2")
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2])
+    winRegions.push("0.0", "1.1", "2.2")
+  if (vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0])
+    winRegions.push("0.2", "1.1", "2.0")
+  return winRegions
+}
+// Desabilita uma região do tabuleiro para que não seja mais clicável
+function disableRegion(element) {
+  element.classList.remove('cursor-pointer')
+  element.removeEventListener('click', handleBoardClick)
+}
+// Pinta as regiões onde o jogador venceu e mostra seu nome na tela
+function handleWin(regions) {
+  regions.forEach(function (region) {
+    document.querySelector('[data-region="' + region + '"]').classList.add('win')
+  })
+  const playerName = document.getElementById(turnPlayer).value
+  document.querySelector('h2').innerHTML = playerName + ' venceu!'
+}
 
-    const h2 = document.createElement('h2')
-    h2.innerText = 'INFORME OS DADOS DOS JOGADORES PARA COMEÇAR!'
-    h2.classList = 'charKey'
-    h2.id = 'titulo'
-
-    const labelPlayer1 = document.createElement('label')
-    labelPlayer1.setAttribute('for' , 'player1')   
-    labelPlayer1.classList = 'charKey'
-    labelPlayer1.innerText = 'Jogador 1: '
-    labelPlayer1.id = "player1"
-
-    const inputPlayer1 = document.createElement('input')
-    inputPlayer1.type = 'text'
-    inputPlayer1.name = 'player1'
-    inputPlayer1.classList = 'charKey'
-
-    const labelPlayer2 = document.createElement('label')
-    labelPlayer2.setAttribute('for' , 'player2')   
-    labelPlayer2.classList = 'charKey'
-    labelPlayer2. id = 'player2'
-    labelPlayer2.innerText = 'Jogador 2: '
-
-    const inputPlayer2 = document.createElement('input')
-    inputPlayer2.type = 'text'
-    inputPlayer2.name = 'player2'
-    inputPlayer2.classList = 'charKey'
-
-    const begin = document.createElement('button')
-    begin.id = 'begin'
-    begin.type = 'button'
-    begin.innerText = 'Começar'
-
-    infoPlayers.append(
-        h1 , h2 , labelPlayer1 , inputPlayer1 , labelPlayer2 , inputPlayer2 , begin
-    )
-
-    begin.addEventListener('click' , function () {
-        infoPlayers.classList.remove('loginPlayers')
-        infoPlayers.remove(h1 , h2 , labelPlayer1 , inputPlayer1 , labelPlayer2 , inputPlayer2 , begin)
-
-        const namePlayer1Value = inputPlayer1.value;
-        namePlayer1.push(namePlayer1Value);
-
-        const namePlayer2Value = inputPlayer2.value;
-        namePlayer2.push(namePlayer2Value)
-
-        console.log(namePlayer1Value)
-        console.log(namePlayer2Value)
-        
-    })
-    
-});
+function handleBoardClick(ev) {
+  // Obtém os índices da região clicada
+  const span = ev.currentTarget
+  const region = span.dataset.region // N.N
+  const rowColumnPair = region.split('.') // ["N", "N"]
+  const row = rowColumnPair[0]
+  const column = rowColumnPair[1]
+  // Marca a região clicada com o símbolo do jogador
+  if (turnPlayer === 'player1') {
+    span.innerText = 'X'
+    vBoard[row][column] = 'X'
+  } else {
+    span.innerText = 'O'
+    vBoard[row][column] = 'O'
+  }
+  // Limpa o console e exibe nosso tabuleiro virtual
+  console.clear()
+  console.table(vBoard)
+  // Desabilita a região clicada
+  disableRegion(span)
+  // Verifica se alguém venceu
+  const winRegions = getWinRegions()
+  if (winRegions.length > 0) {
+    handleWin(winRegions)
+  } else if (vBoard.flat().includes('')) {
+    turnPlayer = turnPlayer === 'player1' ? 'player2' : 'player1'
+    updateTitle()
+  } else {
+    document.querySelector('h2').innerHTML = 'Empate!'
+  }
+}
+// Adiciona o evento no botão que inicia o jogo
+document.getElementById('start').addEventListener('click', initializeGame)
