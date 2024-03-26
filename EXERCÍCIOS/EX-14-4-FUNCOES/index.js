86c46ddca571bb93cf9d2f8d1030f13950db6baf
dionatamartini <dionatamartinii@hotmail.com>
2024-03-26 15:36:20 -0300

    ex14

--- EXERCÍCIOS/EX-14-4-FUNCOES/index.js
+++ EXERCÍCIOS/EX-14-4-FUNCOES/index.js
@@ -1,23 +1,46 @@
 const media = (...media) => {
     const soma = media.reduce((accum , num) => accum + num, 0);
-    const quantidade = media.length;
-    return `A média é ${soma / quantidade}`
+    return `A média é ${soma / media.length}`
 }
 
 console.log(media(2,6,3,7,4))
 
-// o usuário tem que informar o n(número) e p(peso)
-const mediaPonderada = (...args) => {
-    // Separando os números e seus respectivos pesos
-    const nums = args.filter((_, index) => index % 2 === 0);
-    const pesos = args.filter((_, index) => index % 2 !== 0);
+const weightedAverage = (...entries) => {
+    const sum = entries.reduce((accum, { number, weight }) => accum + (number * (weight ?? 1)), 0)
+    const weightSum = entries.reduce((accum, entry) => accum + (entry.weight ?? 1), 0)
+    return sum / weightSum
+  }
+  
+  console.log(`Média Ponderada: ${weightedAverage(
+    { number: 9, weight: 3 },
+    { number: 7 },
+    { number: 10, weight: 1 },
+  )}`)
 
-    // Calculando a média ponderada
-    const somaProdutos = nums.reduce((accum, num, index) => accum + num * pesos[index], 0);
-    const somaPesos = pesos.reduce((accum, peso) => accum + peso, 0);
-    const media = somaProdutos / somaPesos;
 
-    return `A média ponderada de ${nums} com pesos ${pesos} é ${media}`;
-}
+  const median = (...numbers) => {
+    const orderedNumbers = [...numbers].sort((a, b) => a - b)
+    const middle = Math.floor(orderedNumbers.length / 2)
+    if (orderedNumbers.length % 2 !== 0) {
+      return orderedNumbers[middle]
+    }
+    const firstMedian = orderedNumbers[middle - 1]
+    const secondMedian = orderedNumbers[middle]
+    return average(firstMedian, secondMedian)
+  }
+  
+  console.log(`Mediana: ${median(2, 5, 99, 4, 42, 7)}`)
+  console.log(`Mediana: ${median(15, 14, 8, 7, 3)}`)
 
-console.log(mediaPonderada(7, 2, 9, 5, 3, 1));
+
+  const mode = (...numbers) => {
+    // [ [n, qtd], [n, qtd], [n, qtd] ]
+    const quantities = numbers.map(num => [
+      num,
+      numbers.filter(n => num === n).length
+    ])
+    quantities.sort((a, b) => b[1] - a[1])
+    return quantities[0][0]
+  }
+  
+  console.log(mode(1, 1, 99,99,99,99,99,99,99,99, 5, 4, 9, 7, 4, 3, 5, 2, 4, 0, 4))
